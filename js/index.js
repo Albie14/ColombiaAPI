2a4b84d46d22b53417dbc2d7b61dfa5d88ee772a
Albie <albiesilva@gmail.com>
2025-10-14 19:06:44 -0400

    carga completa archivos

--- js/index.js
+++ js/index.js
@@ -3,26 +3,47 @@
 const video = document.getElementById('video-main');
 video.playbackRate = 0.5; // Cambia el valor a la velocidad deseada (0.5 es la mitad de la velocidad normal)
 
-//btn abrir y cerrar menu
+//btn abrir y cerrar menu nav
 
-const btnCerrar = document.getElementById('btn-cerrar')
-const btnAbrir = document.getElementById('btn-abrir')
-const menu = document.querySelector('.plegable');
+const btnCerrarMenuNav = document.getElementById('btn-cerrar')
+const btnAbrirMenuNav = document.getElementById('btn-abrir')
+const menuNav = document.querySelector('.plegable');
 
-btnAbrir.addEventListener('click', ()=>{
-    menu.style.display = 'block';
-    btnAbrir.style.display = 'none';
-    btnCerrar.style.display = 'flex';
+btnAbrirMenuNav.addEventListener('click', ()=>{
+    menuNav.style.display = 'block';
+    btnAbrirMenuNav.style.display = 'none';
+    btnCerrarMenuNav.style.display = 'flex';
 })
 
-menu.addEventListener('mouseleave', ()=>{
-    menu.style.display = 'none';
-    btnAbrir.style.display = 'flex';
-    btnCerrar.style.display = 'none';
+menuNav.addEventListener('mouseleave', ()=>{
+    menuNav.style.display = 'none';
+    btnAbrirMenuNav.style.display = 'flex';
+    btnCerrarMenuNav.style.display = 'none';
 })
-btnCerrar.addEventListener('click', ()=>{
-    menu.style.display = 'none';
-    btnAbrir.style.display = 'flex';
-    btnCerrar.style.display = 'none';
+btnCerrarMenuNav.addEventListener('click', ()=>{
+    menuNav.style.display = 'none';
+    btnAbrirMenuNav.style.display = 'flex';
+    btnCerrarMenuNav.style.display = 'none';
 
 }) 
+
+//ampliar menu en responsive
+const btnMenuHamburguesa = document.getElementById('btn-abrir-menu');
+const btnCerrarMenuHamburguesa = document.getElementById('btn-cerrar-menu');
+const navMenuResponsive = document.querySelector('.nav');
+
+
+btnMenuHamburguesa.addEventListener('click', ()=>{
+    navMenuResponsive.style.display = 'flex';
+    btnMenuHamburguesa.style.display = 'none';
+    btnCerrarMenuHamburguesa.style.display = 'flex';
+})
+
+btnCerrarMenuHamburguesa.addEventListener('click', ()=>{
+    navMenuResponsive.style.display = 'none';
+    btnMenuHamburguesa.style.display = 'flex';
+    btnCerrarMenuHamburguesa.style.display = 'none';
+    menuNav.style.display = 'none';
+    btnAbrirMenuNav.style.display = 'flex';
+    btnCerrarMenuNav.style.display = 'none'
+})
